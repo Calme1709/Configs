689a7f7d23135c4827124b3755ce5bdfda335646
Calme1709 <callumlaw1709@outlook.com>
2020-12-19 21:38:34 +1300

    Back: Fix typo

--- packages/back/.eslintrc.js
+++ packages/back/.eslintrc.js
@@ -1,3 +1,3 @@
-const { CommonESLint: ESLint, deepMerge } = require("@calme1709/config-common");
+const { ESlint: CommonESLint, deepMerge } = require("@calme1709/config-common");
 
 module.exports = deepMerge(CommonESLint, { env: { node: true }});
