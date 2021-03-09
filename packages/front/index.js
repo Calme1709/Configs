c3607834ef554e9130ffa5739b751199e9bc2baf
Calme1709 <callumlaw1709@outlook.com>
2021-03-09 20:42:46 +1300

    Front: Add way to specify custom entry point

--- packages/front/index.js
+++ packages/front/index.js
@@ -4,13 +4,15 @@ const path = require("path");
 
 let executingProcess;
 
+const file = process.argv.length > 3 ? process.argv[3] : null;
+
 switch(process.argv[2]){
 	case "start":
-		executingProcess = exec(`npx webpack serve --config "${path.join(__dirname, "webpack/development.js")}"`);
+		executingProcess = exec(`npx webpack serve ${file === null ? "" : `--entry ${path.join(__dirname, `../../../src/${file}`)}`} --config "${path.join(__dirname, "webpack/development.js")}"`);
 		
 		break;
 	case "build":
-		executingProcess = exec(`npx webpack --config "${path.join(__dirname, "webpack/production.js")}"`);
+		executingProcess = exec(`npx webpack ${file === null ? "" : `--entry ${path.join(__dirname, `../../../src/${file}`)}`} --config "${path.join(__dirname, "webpack/production.js")}"`);
 		
 		break;
 	
